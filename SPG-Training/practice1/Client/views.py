43948370d4b49b246236ecc0b68b79bce82bca47
Joseph Rios Henao <joseph232005@gmail.com>
2022-02-14 00:12:55 -0500

    finish views

--- SPG-Training/practice1/Client/views.py
+++ SPG-Training/practice1/Client/views.py
@@ -1,3 +1,120 @@
+# RENDER DEFAULT
 from django.shortcuts import render
+# CRUD METHODS
+from django.views.generic import DetailView, ListView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+# DATABASE MODELS
+from .models import *
+# FORMS
+from django import forms
+# Nos sirve para redireccionar despues de una acción revertiendo patrones de expresiones regulares 
+from django.urls import reverse
+# Habilitamos el uso de mensajes en Django
+from django.contrib import messages 
+# Habilitamos los mensajes para class-based views 
+from django.contrib.messages.views import SuccessMessageMixin 
 
-# Create your views here.
+class ListState(ListView):
+    model = State
+class CreateState(SuccessMessageMixin, CreateView):
+    model = State
+    form = State
+    fields = "__all__"
+    success_message = "State created successfully"
+    def get_success_url(self):        
+        return reverse('state')
+class ReadState(DetailView):
+    model = State
+class UpdateState(SuccessMessageMixin, UpdateView):
+    model = State
+    form = State
+    fields = "__all__"
+    success_message = "State updated successfully"
+    def get_success_url(self):        
+        return reverse('State') 
+class DeleteState(SuccessMessageMixin, DeleteView):
+    model = State
+    form = State
+    fields = "__all__"
+    success_message = 'State deleted successfully' 
+    def get_success_url(self):   
+        return reverse('State')
+
+class ListTypeAct(ListView):
+    model = TypeAct
+class CreateTypeAct(SuccessMessageMixin, CreateView):
+    model = TypeAct
+    form = TypeAct
+    fields = "__all__"
+    success_message = "TypeAct created successfully"
+    def get_success_url(self):        
+        return reverse('TypeAct')
+class ReadTypeAct(DetailView):
+    model = TypeAct
+class UpdateTypeAct(SuccessMessageMixin, UpdateView):
+    model = TypeAct
+    form = TypeAct
+    fields = "__all__"
+    success_message = "TypeAct updated successfully"
+    def get_success_url(self):        
+        return reverse('TypeAct') 
+class DeleteTypeAct(SuccessMessageMixin, DeleteView):
+    model = TypeAct
+    form = TypeAct
+    fields = "__all__"
+    success_message = 'TypeAct deleted successfully' 
+    def get_success_url(self):   
+        return reverse('TypeAct')
+
+class ListTypeDoc(ListView):
+    model = TypeDoc
+class CreateTypeDoc(SuccessMessageMixin, CreateView):
+    model = TypeDoc
+    form = TypeDoc
+    fields = "__all__"
+    success_message = "TypeDoc created successfully"
+    def get_success_url(self):        
+        return reverse('TypeDoc')
+class ReadTypeDoc(DetailView):
+    model = TypeDoc
+class UpdateTypeDoc(SuccessMessageMixin, UpdateView):
+    model = TypeDoc
+    form = TypeDoc
+    fields = "__all__"
+    success_message = "TypeDoc updated successfully"
+    def get_success_url(self):        
+        return reverse('TypeDoc') 
+class DeleteTypeDoc(SuccessMessageMixin, DeleteView):
+    model = TypeDoc
+    form = TypeDoc
+    fields = "__all__"
+    success_message = 'TypeDoc deleted successfully' 
+    def get_success_url(self):   
+        return reverse('TypeDoc')
+
+
+class ListClient(ListView):
+    model = Client
+class CreateClient(SuccessMessageMixin, CreateView):
+    model = Client
+    form = Client
+    fields = "__all__"
+    success_message = "Client created successfully"
+    def get_success_url(self):        
+        return reverse('Client')
+class ReadClient(DetailView):
+    model = Client
+class UpdateClient(SuccessMessageMixin, UpdateView):
+    model = Client
+    form = Client
+    fields = "__all__"
+    success_message = "Client updated successfully"
+    def get_success_url(self):        
+        return reverse('Client') 
+class DeleteClient(SuccessMessageMixin, DeleteView):
+    model = Client
+    form = Client
+    fields = "__all__"
+    success_message = 'Client deleted successfully' 
+    def get_success_url(self):   
+        return reverse('Client')
--- SPG-Training/practice1/Service/views.py
+++ SPG-Training/practice1/Service/views.py
@@ -1,3 +1,123 @@
+# RENDER DEFAULT
 from django.shortcuts import render
+# CRUD METHODS
+from django.views.generic import DetailView, ListView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+# DATABASE MODELS
+from .models import *
+# FORMS
+from django import forms
+# Nos sirve para redireccionar despues de una acción revertiendo patrones de expresiones regulares 
+from django.urls import reverse
+# Habilitamos el uso de mensajes en Django
+from django.contrib import messages 
+# Habilitamos los mensajes para class-based views 
+from django.contrib.messages.views import SuccessMessageMixin 
 
-# Create your views here.
+
+class ListMovProdServ(ListView):
+    model = MovProdServ
+class CreateMovProdServ(SuccessMessageMixin, CreateView):
+    model = MovProdServ
+    form = MovProdServ
+    fields = "__all__"
+    success_message = "MovProdServ created successfully"
+    def get_success_url(self):        
+        return reverse('MovProdServ')
+class ReadMovProdServ(DetailView):
+    model = MovProdServ
+class UpdateMovProdServ(SuccessMessageMixin, UpdateView):
+    model = MovProdServ
+    form = MovProdServ
+    fields = "__all__"
+    success_message = "MovProdServ updated successfully"
+    def get_success_url(self):        
+        return reverse('MovProdServ') 
+class DeleteMovProdServ(SuccessMessageMixin, DeleteView):
+    model = MovProdServ
+    form = MovProdServ
+    fields = "__all__"
+    success_message = 'MovProdServ deleted successfully' 
+    def get_success_url(self):   
+        return reverse('MovProdServ')
+
+
+class ListTypeMovement(ListView):
+    model = TypeMovement
+class CreateTypeMovement(SuccessMessageMixin, CreateView):
+    model = TypeMovement
+    form = TypeMovement
+    fields = "__all__"
+    success_message = "TypeMovement created successfully"
+    def get_success_url(self):        
+        return reverse('TypeMovement')
+class ReadTypeMovement(DetailView):
+    model = TypeMovement
+class UpdateTypeMovement(SuccessMessageMixin, UpdateView):
+    model = TypeMovement
+    form = TypeMovement
+    fields = "__all__"
+    success_message = "TypeMovement updated successfully"
+    def get_success_url(self):        
+        return reverse('TypeMovement') 
+class DeleteTypeMovement(SuccessMessageMixin, DeleteView):
+    model = TypeMovement
+    form = TypeMovement
+    fields = "__all__"
+    success_message = 'TypeMovement deleted successfully' 
+    def get_success_url(self):   
+        return reverse('TypeMovement')
+
+
+class ListServicesProduct(ListView):
+    model = ServicesProduct
+class CreateServicesProduct(SuccessMessageMixin, CreateView):
+    model = ServicesProduct
+    form = ServicesProduct
+    fields = "__all__"
+    success_message = "ServicesProduct created successfully"
+    def get_success_url(self):        
+        return reverse('ServicesProduct')
+class ReadServicesProduct(DetailView):
+    model = ServicesProduct
+class UpdateServicesProduct(SuccessMessageMixin, UpdateView):
+    model = ServicesProduct
+    form = ServicesProduct
+    fields = "__all__"
+    success_message = "ServicesProduct updated successfully"
+    def get_success_url(self):        
+        return reverse('ServicesProduct') 
+class DeleteServicesProduct(SuccessMessageMixin, DeleteView):
+    model = ServicesProduct
+    form = ServicesProduct
+    fields = "__all__"
+    success_message = 'ServicesProduct deleted successfully' 
+    def get_success_url(self):   
+        return reverse('ServicesProduct')
+
+
+class ListServiceProductType(ListView):
+    model = ServiceProductType
+class CreateServiceProductType(SuccessMessageMixin, CreateView):
+    model = ServiceProductType
+    form = ServiceProductType
+    fields = "__all__"
+    success_message = "ServiceProductType created successfully"
+    def get_success_url(self):        
+        return reverse('ServiceProductType')
+class ReadServiceProductType(DetailView):
+    model = ServiceProductType
+class UpdateServiceProductType(SuccessMessageMixin, UpdateView):
+    model = ServiceProductType
+    form = ServiceProductType
+    fields = "__all__"
+    success_message = "ServiceProductType updated successfully"
+    def get_success_url(self):        
+        return reverse('ServiceProductType') 
+class DeleteServiceProductType(SuccessMessageMixin, DeleteView):
+    model = ServiceProductType
+    form = ServiceProductType
+    fields = "__all__"
+    success_message = 'ServiceProductType deleted successfully' 
+    def get_success_url(self):   
+        return reverse('ServiceProductType')
