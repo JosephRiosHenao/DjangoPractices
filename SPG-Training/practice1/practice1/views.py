397aa644715a719ee591e3e04221ee6ee4b9980f
Joseph Rios Henao <joseph232005@gmail.com>
2022-02-19 10:55:58 -0500

    finish practice

--- SPG-Training/practice1/practice1/views.py
+++ SPG-Training/practice1/practice1/views.py
@@ -1,4 +1,4 @@
 from django.shortcuts import render
 
 def HomePage(request):
-    return render(request, 'pages/home.html', {})
+    return render(request, 'pages/home.html', {"homePageState":True})
