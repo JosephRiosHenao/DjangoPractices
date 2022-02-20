ade7809a8791ddfec13d87e32bbb6ed418899637
Joseph Rios Henao <joseph232005@gmail.com>
2022-02-20 17:02:56 -0500

    procced with migrate

--- practice3/profiles/models.py
+++ practice3/profiles/models.py
@@ -1,3 +1,8 @@
 from django.db import models
-
+from authors.models import Author
 # Create your models here.
+class Profile(models.Model):
+    alias = models.CharField(max_length=50)
+    author = models.OneToOneField(Author, on_delete=models.CASCADE, null=False, blank=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    def __str__(self): return self.alias
